7c882028b858e371351d2e75a0d44e27601c8904
Mark Silverio <dashplaygrounds@gmail.com>
2024-07-16 11:28:07 +0800

    feat: added new methods

--- src/main/java/com/tjdev/gcashapp/utils/CashTransfer.java
+++ src/main/java/com/tjdev/gcashapp/utils/CashTransfer.java
@@ -1,4 +1,10 @@
 package com.tjdev.gcashapp.utils;
 
 public class CashTransfer {
+
+    public boolean cashTransfer(Long sourceId,
+                                Long userId,
+                                Double amount) {
+        return false;
+    }
 }
--- src/main/java/com/tjdev/gcashapp/utils/Transactions.java
+++ src/main/java/com/tjdev/gcashapp/utils/Transactions.java
@@ -1,4 +1,15 @@
 package com.tjdev.gcashapp.utils;
 
 public class Transactions {
+    public void viewAll() {
+        // TODO
+    }
+
+    public void viewUserAll(Long userId) {
+        // TODO
+    }
+
+    public void viewTransaction() {
+        // TODO
+    }
 }
